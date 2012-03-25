303157f1d1a15f37c0546ea16d40b20546a68d5c
Pankaj More <pankajmore@gmail.com>
2012-03-25 13:15:03 +0530

    file header of btree

---
+++ src/Data/BPlusTree/BPlusFileDescriptor.hs
@@ -0,0 +1,50 @@
+module Data.BPlusTree.BPlusFileDescriptor where
+
+-- | Structure of B-tree on block storage
+-- The full B-tree is stored in a single file
+-- A B-tree file has a fixed blocksize, which is always a multiple of
+-- 512 bytes. The first block in the file is a header block:
+
+-- Header block:
+--  4 bytes - block size
+--  4 bytes - index of "root" blockSize
+--  What else?
+
+import Data.Word (Word32)
+import Data.Binary
+import qualified Data.ByteString.Lazy as B
+import System.IO
+
+data BPlusFileDescriptor = BPFD { blockSize :: Word32,
+                                  rootIndex :: Word32 }
+                           deriving (Show,Eq)
+
+instance Binary BPlusFileDescriptor where
+    put bpfd = do 
+        put (blockSize bpfd)
+        put (rootIndex bpfd)
+
+    get = do
+        s <- get :: Get Word32
+        i <- get :: Get Word32
+        return (BPFD s i)
+
+
+readBPlusHeader :: FilePath -> IO BPlusFileDescriptor
+readBPlusHeader fp = do
+    bs <- B.readFile fp
+    let bpfd = decode bs :: BPlusFileDescriptor
+    return bpfd
+      
+writeBPlusHeader :: BPlusFileDescriptor -> FilePath -> IO ()
+writeBPlusHeader bpfd fp = do
+    let bs = encode bpfd
+    B.writeFile fp bs
+
+test = do
+    let bpfd = BPFD (512::Word32) (0::Word32)
+    print bpfd
+    writeBPlusHeader bpfd "test.btree"
+    s <- readBPlusHeader "test.btree"
+    print s
+    print (s == bpfd)
--- src/Data/BPlusTree/BPlusTree.hs
+++ src/Data/BPlusTree/BPlusTree.hs
@@ -1,9 +1,43 @@
 module BPlusTree where
 
--- | Implementation of a B+ Tree
+-- | Implementation of B+ Tree, using a RecordFile as the underlying
+-- primitive. A B+ Tree is a BTree where all the actual data is in the
+-- leafs.
+
+-- There are two kinds of nodes, leaf nodes and internal nodes. An
+-- internal node contains only keys + pointers to other nodes. A leaf
+-- node contains only keys + actual data corresponding to each key.
+
+-- An internal node takes the following structure:
+--
+-- [ <p1> | kA | <p2> | kB | <p3> | kC | <p4> ]
+--
+-- where, e.g., <p1> points to the place to search for all keys less
+-- than or equal to kA, <p2> points to the place to search for all
+-- keys > kA and <= kB, and <p4> to the place to search for keys > kC.
+--
+-- The ps may either point to another internal node, for more searching, or
+-- a leaf node.
+
+-- A leaf node takes the structure:
+--
+-- [ kA | vA | kB | vB | kC | vC | <p> ]
+--
+-- where the 'vA' is the value corresponding to 'kA' and <p> points to
+-- the next leaf node along.
+
+import qualified Data.ByteString.Char8 as B
+import Data.ByteString.Char8 (ByteString)
+import Data.BPlusTree.BPlusFileDescriptor
 
 type Key = ByteString
 type Value = ByteString
 
+newtype BPPtr = BPP Int deriving (Show,Eq)
+
 data BPlusNode = BPLeaf [(Key, Value)] (Maybe BPPtr)
                | BPInternal [(Key, BPPtr)] BPPtr
+                 deriving (Show)
+
+
+
